7197d18b78a18d41367720f2a1ebbb60790163cf
nawazsk <nawaz.540@gmail.com>
2020-04-10 18:39:46 +0530

    backend update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 A blog created using next js and netlify cms
+
+
+Implementation
+
+https://github.com/masives/netlifycms-nextjs
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head"
 import { Component } from 'react'
 import { attributes, react as HomeContent } from '../content/home.md';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Button } from 'react-bootstrap';
 
 export default class Home extends Component {
   render() {
@@ -9,6 +12,12 @@ export default class Home extends Component {
       <>
         <Head>
           <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+            crossOrigin="anonymous"
+            />
         </Head>
         <article>
           <h1>{title}</h1>
